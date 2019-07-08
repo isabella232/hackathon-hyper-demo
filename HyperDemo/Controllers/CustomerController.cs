1985ea26de2f2e8714ef9d0424ec87abd32f19d1
Magnus Krona <magnus.krona@gmail.com>
2019-07-08 10:38:37 +0200

    Add project files.

---
+++ HyperDemo/Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HyperDemo.Controllers
+{
+    [Authorize]
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Customer id
+        [Route("{id:int}")]
+        public ActionResult Get(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ HyperDemo/Helpers/JWTHelper.cs
@@ -0,0 +1,32 @@
+﻿using JWT.Algorithms;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HyperDemo.Helpers
+{
+    public static class JWTHelper
+    {
+
+        private const string SHARED_SECRET = "hackathon2019_secret";
+
+        public static string GenerateJWT()
+        {
+            var userId = HttpContext.Current.User.Identity.GetUserId();
+            var manager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var currentUser = manager.FindById(userId);
+
+            return new JWT.Builder.JwtBuilder()
+                    .WithAlgorithm(new HMACSHA256Algorithm())
+                    .WithSecret(SHARED_SECRET)
+                    .AddClaim("iss", "hyperdemo_hackathon")
+                    .AddClaim("sub", currentUser.Id)
+                    .AddClaim("email", currentUser.Email)
+                    .AddClaim("exp", DateTimeOffset.UtcNow.AddSeconds(10).ToUnixTimeSeconds())
+                    .Build();
+        }
+    }
+}
